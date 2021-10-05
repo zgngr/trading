63e97c9ffe8907370e6185bc893c07b6feb14710
Özgün Gör <zgn.gor@gmail.com>
2021-10-05 21:52:12 +0000

    changes

--- strategies/buy_hold.py
+++ strategies/buy_hold.py
@@ -4,12 +4,27 @@ import math
 
 
 class BuyHold(bt.Strategy):
+    
+    params = dict(
+        verbose=False
+    )
+
+    def __init__(self):
+        self.order = None
+        self.val_start = self.broker.get_cash()
+        self.dataclose = self.datas[0].close
+
+    def log(self, txt, dt=None):
+        if self.p.verbose:
+            dt = dt or self.datas[0].datetime.date(0)
+            print('%s, %s' % (dt.isoformat(), txt))
 
     def start(self):
         self.val_start = self.broker.get_cash()
 
     def nextstart(self):
         size = math.floor((0.99 * self.broker.cash) / self.data.close)
+        self.log('BUY CREATE, %.2f' % self.dataclose[0])
         self.buy(size=size)
 
 
